9044a2733c01ee2fdd7bfa1e74852b23a910c924
Buffet Time <radd86@gmail.com>
2018-01-24 15:32:45 -0500

    minor html and css changes

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Optional } from '@angular/core';
+import { Component, Optional, Inject } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material';
 
 
@@ -14,23 +14,21 @@ export class AppComponent {
   writeProtection: boolean;
   rowsOfSongs: number = 1;
 
-  constructor(private _dialog: MatDialog) {}
-
+  constructor(private dialog: MatDialog) {}
   
   openSettings() {
-    const dialogRef = this._dialog.open(DialogContentComponent);
+    const dialogRef = this.dialog.open(DialogContentComponent);
 
     dialogRef.afterClosed().subscribe(result => {
         // do things after the dialog closes
     });
   }
-  
 }
 
 
 // main settings dialog
 @Component({
-  selector: 'app-root',
+  selector: 'setting-dialog',
   templateUrl: '../dialogs/main.settings.dialog.html',
   styleUrls: ['../dialogs/main.settings.dialog.css']
 })
